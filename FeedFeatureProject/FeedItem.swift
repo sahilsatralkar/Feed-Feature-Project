cd7e1a96f75b7e29ac76f34179faf0c8342a1b4b
Sahil Satralkar <sahil.satralkar@gmail.com>
2023-05-08 13:15:31 +0530

    Created FeedItem response model

---
+++ FeedFeatureProject/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  FeedFeatureProject
+//
+//  Created by Sahil Satralkar on 08/05/23.
+//
+
+import Foundation
+
+
+struct FeedItem {
+    let id: String
+    let description: String?
+    let location: String?
+    let image: URL
+}
