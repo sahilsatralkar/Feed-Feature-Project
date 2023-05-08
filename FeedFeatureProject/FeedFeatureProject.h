b09a10dcb31f52f809b728b5854c2a1cbac70edd
Sahil Satralkar <sahil.satralkar@gmail.com>
2023-05-08 13:13:10 +0530

    Initial Commit

---
+++ FeedFeatureProject/FeedFeatureProject.h
@@ -0,0 +1,18 @@
+//
+//  FeedFeatureProject.h
+//  FeedFeatureProject
+//
+//  Created by Sahil Satralkar on 08/05/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FeedFeatureProject.
+FOUNDATION_EXPORT double FeedFeatureProjectVersionNumber;
+
+//! Project version string for FeedFeatureProject.
+FOUNDATION_EXPORT const unsigned char FeedFeatureProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FeedFeatureProject/PublicHeader.h>
+
+
